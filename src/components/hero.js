3feb532d4ef2baebfb69041da5efeafad554a773
Adam Garling <33332581+argarlin24@users.noreply.github.com>
2019-11-13 15:13:51 -0800

    updated final hero img

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,17 +4,24 @@ import styled from "styled-components"
 import Fade from "react-reveal/Fade"
 import { Flex, Box } from "reflexbox/styled-components"
 import { MainHeading, SubHeading } from "../elements"
-import pwaImg from "../images/pwa-colors.svg"
+import { below } from "../utilities/breakpoints"
+import pwaImg from "../images/pwa-transparent.svg"
 
 const HeroWrapper = styled(Flex)`
-    background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)),
-        url(${pwaImg});
+    background: url(${pwaImg});
     background-position: center;
     background-repeat: no-repeat;
-    background-size: contain;
+    background-size: 50%;
+    background-position: 50% 75%;
     color: ${props => props.theme.colors.black};
     min-height: 100vh;
     padding-top: 15vh;
+    ${below.med`
+         background-size: 75%;
+  `}
+    ${below.small`
+         padding-top: 10vh;
+  `}
 `
 
 const HeadingBox = styled(Box)`
@@ -22,22 +29,24 @@ const HeadingBox = styled(Box)`
 `
 
 const Hero = () => (
-    <HeroWrapper
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="start"
-    >
-        <HeadingBox
-            width={["75%", "75%", "50%", "50%"]}
-            bg="rgba(0,0,0,.5)"
-            padding="75px 0"
+    <Box bg="#3e66a4">
+        <HeroWrapper
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="start"
         >
-            <Fade left cascade>
-                <MainHeading primary>ADAM GARLING</MainHeading>
-                <SubHeading primary>Full Stack Web Developer</SubHeading>
-            </Fade>
-        </HeadingBox>
-    </HeroWrapper>
+            <HeadingBox
+                width={["75%", "75%", "50%", "50%"]}
+                // bg="rgba(0,0,0,.5)"
+                padding="75px 0"
+            >
+                <Fade left cascade>
+                    <MainHeading primary>ADAM GARLING</MainHeading>
+                    <SubHeading primary>Full Stack Web Developer</SubHeading>
+                </Fade>
+            </HeadingBox>
+        </HeroWrapper>
+    </Box>
 )
 
 export default Hero
