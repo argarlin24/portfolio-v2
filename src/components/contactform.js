cbba6f1587d45aee160eba5222856b6744825d93
Adam Garling <33332581+argarlin24@users.noreply.github.com>
2019-12-04 21:25:59 -0800

    updated contact form for gatsby

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 import { Box, Flex } from "reflexbox/styled-components"
 import { PrimaryBtn } from "../elements"
 import { below } from "../utilities/breakpoints"
+import { navigate } from "gatsby-link"
 
 const StyledFlex = styled(Flex)`
     background: ${props => props.theme.colors.white};
@@ -43,40 +44,79 @@ const BlockLabel = styled.label`
     font-weight: bold;
     color: ${props => props.theme.colors.black};
 `
+function encode(data) {
+    return Object.keys(data)
+        .map(
+            key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+        )
+        .join("&")
+}
 
-const ContactForm = () => (
-    <StyledFlex flexDirection="column" alignItems="center">
-       <Box width={"75%"} display="flex" justifyContent="center">
-                    <h1>CONTACT</h1>
-                </Box>
-        <Box width="75%" display="flex" justifyContent="center">
-            <div>
-                <form
-                    name="contact"
-                    method="POST"
-                    netlify-honeypot="bot-field"
-                    data-netlify-recaptcha="true"
-                    data-netlify="true"
-                >
-                    <BlockLabel>EMAIL:</BlockLabel>
+const ContactForm = () => {
+    const [state, setState] = React.useState({})
 
-                    <Input type="email" name="email" />
+    const handleChange = e => {
+        setState({ ...state, [e.target.name]: e.target.value })
+    }
 
-                    <BlockLabel>MESSAGE:</BlockLabel>
+    const handleSubmit = e => {
+        e.preventDefault()
+        const form = e.target
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({
+                "form-name": form.getAttribute("name"),
+                ...state,
+            }),
+        })
+            .then(() => navigate(form.getAttribute("action")))
+            .catch(error => alert(error))
+    }
+    return (
+        <StyledFlex flexDirection="column" alignItems="center">
+            <Box width={"75%"} display="flex" justifyContent="center">
+                <h1>CONTACT</h1>
+            </Box>
+            <Box width="75%" display="flex" justifyContent="center">
+                <div>
+                    <form
+                        name="contact"
+                        method="post"
+                        netlify-honeypot="bot-field"
+                        data-netlify="true"
+                        onSubmit={handleSubmit}
+                    >
+                        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                        <Hidden>
+                            <input name="form-name" value="contact" />
+                        </Hidden>
+                        <BlockLabel>EMAIL:</BlockLabel>
 
-                    <TextArea name="message" rows="10" />
-                    <Hidden>
-                        <label>Don’t fill this out if you're human:</label>
-                        <input name="bot-field" />
-                    </Hidden>
+                        <Input
+                            type="email"
+                            name="email"
+                            onChange={handleChange}
+                        />
 
-                    <div data-netlify-recaptcha="true" />
+                        <BlockLabel>MESSAGE:</BlockLabel>
 
-                    <PrimaryBtn type="submit">SUBMIT</PrimaryBtn>
-                </form>
-            </div>
-        </Box>
-    </StyledFlex>
-)
+                        <TextArea
+                            name="message"
+                            rows="10"
+                            onChange={handleChange}
+                        />
+                        <Hidden>
+                            <label>Don’t fill this out if you're human:</label>
+                            <input name="bot-field" onChange={handleChange} />
+                        </Hidden>
+
+                        <PrimaryBtn type="submit">SUBMIT</PrimaryBtn>
+                    </form>
+                </div>
+            </Box>
+        </StyledFlex>
+    )
+}
 
 export default ContactForm
